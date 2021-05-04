30f21e4cbc8ad8acb65147ce01a677c2f82a439b
Matheus Pellizzon <matheusp1@al.insper.edu.br>
2021-05-04 13:56:31 -0300

    normalizacao simples de texto (lower)

--- scripts/make_archive_from_elon_tweets.py
+++ scripts/make_archive_from_elon_tweets.py
@@ -10,16 +10,17 @@ from nltk.tokenize import TweetTokenizer
 from se.archive import save_archive
 
 
-MSG_DESCRIPTION = '''Le arquivo de tweets e gera JSON com tweets tokenizados.
+MSG_DESCRIPTION = """Le arquivo de tweets e gera JSON com tweets tokenizados.
 Tweets obtidos de https://www.kaggle.com/ayhmrba/elon-musk-tweets-2010-2021
-'''
+"""
 
 
 def read_donald_tweets(path):
     df = pd.read_csv(path)
     docs = []
-    tokenizer = TweetTokenizer()
-    for text in df['tweet']:
+    # preserve_case = False: tokenizer will downcase everything except for emoticons
+    tokenizer = TweetTokenizer(preserve_case=False)
+    for text in df["tweet"]:
         toks = tokenizer.tokenize(text)
         docs.append(toks)
     return docs
@@ -27,13 +28,13 @@ def read_donald_tweets(path):
 
 def main():
     parser = ArgumentParser(description=MSG_DESCRIPTION)
-    parser.add_argument('filename_tweets', help='Os tweet.')
-    parser.add_argument('filename_archive', help='Onde guarda.')
+    parser.add_argument("filename_tweets", help="Os tweet.")
+    parser.add_argument("filename_archive", help="Onde guarda.")
     args = parser.parse_args()
 
     docs = read_donald_tweets(args.filename_tweets)
     save_archive(docs, args.filename_archive)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
